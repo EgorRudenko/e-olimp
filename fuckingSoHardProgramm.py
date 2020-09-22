02b33a7a3f20b2ea50b77f4ac54c349a05e39757
EgorRudenko <rudenko.egorka@gmail.com>
2020-09-22 10:08:04 +0300

    I works, but not awful.

--- fuckingSoHardProgramm.py
+++ fuckingSoHardProgramm.py
@@ -10,10 +10,13 @@ side = pow(fullCubes, 1/3)
 width = math.floor(side/2)
 
 if (side % 2) == 0:
-    pass
+    if n == 8:
+        print()
+    elif noFullCubes == 0:
+        print( (2 * ( (side-2) + 1) + 3 * (side - 2)) + (8 * 3) + ( ( ( side - 1 ) ** 2) * ( ( side - 1 ) ** 2 ) ) )
+
 else:
     if n == 1:
         print(12)
     elif noFullCubes == 0:
-        oneSide = (side-1)**2 + (side-2)**2
-        print(oneSide)
+        print( (2 * ( (side - 2) + 1) + 3 * (side - 2)) + (8 * 3) + ( ( ( side - 1 ) ** 2) *  ( ( side - 1 ) ** 2 ) ) )
